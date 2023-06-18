38e056446c12c95bbdc611ad9edeae4af42c688f
washington.riega <washington.riega@itriad.org.br>
2023-06-18 14:36:30 -0400

    TP1

---
+++ TAP/JavaApplication1/src/com/mycompany/tp1/TP1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp1;
+
+/**
+ *
+ * @author washington.riega
+ */
+public class TP1 {
+    
+    public static void main(String[] args) {
+        MainView tela = new MainView();
+        tela.show();
+    }
+}
