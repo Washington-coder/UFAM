015b90378af9a53ac4847aa429ef5944b2e1dbdd
washington.riega <washington.riega@itriad.org.br>
2023-06-18 20:39:16 -0400

    artigos view

--- TAP/TP1/JavaApplication1/src/com/mycompany/rit/RitView.java
+++ TAP/TP1/JavaApplication1/src/com/mycompany/rit/RitView.java
@@ -5,6 +5,7 @@
 package com.mycompany.rit;
 
 import com.mycompany.alunos.AlunoView;
+import com.mycompany.artigos.ArtigosView;
 import com.mycompany.disciplina.DisciplinaView;
 
 /**
@@ -123,6 +124,8 @@ public class RitView extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        ArtigosView tela = new ArtigosView(professor_id);
+        tela.show();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
